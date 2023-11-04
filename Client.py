6aee8be29c1205c9d5f9df16b611e956231adc5b
sung <63391638+sung01299@users.noreply.github.com>
2023-11-04 23:02:17 +0800

    update server/client

--- Client.py
+++ Client.py
@@ -2,11 +2,16 @@ import socket
 import sys
 
 class GameClient:
+    # Initialize variables
     def __init__(self, server_addr, server_port):
+        # Stores server address
         self.server_addr = server_addr
+        # Stores server port number
         self.server_port = int(server_port)
+        # Client socket
         self.client_socket = None
 
+    # Connects to the server
     def connect(self):
         try:
             self.client_socket = socket.socket()
@@ -15,76 +20,107 @@ class GameClient:
             print("Socket error:", emsg)
             sys.exit(1)
 
+    # 1. User Authentication
     def authenticate(self):
         auth = False
+        # Repeats authentication process until user successfully sign in
         while not auth:
             try:
+                # Get username / password
                 username = input("Please input your user name:\n")
                 password = input("Please input your password:\n")
             except Exception as error:
                 print("An exception occured:", error)
-
+            # Communicates with server
             auth_msg = f"/login {username} {password}"
             self.client_socket.send(auth_msg.encode())
-            rcved_authmsg = self.client_socket.recv(1024).decode()
-            print(rcved_authmsg)
-            auth_msg_key = rcved_authmsg.split()[0]
+            rcved_auth_msg = self.client_socket.recv(1024).decode()
+            print(rcved_auth_msg)
+            auth_msg_key = rcved_auth_msg.split()[0]
             if auth_msg_key == "1001":
                 auth = True
 
+    # Dealing the guess by users
     def guess(self, client_msg):
-        guess_input = input("")
+        guess_key = ""
+        guess_input_length = 0
+        client_guess = ""
+        choices = ["true", "false"]
+        # Handles exception when user inputs its guess incorrectly
+        while guess_key != "/guess" or guess_input_length != 2 or client_guess not in choices:
+            guess_input = input("")
+            # Update guess_key, client_guess, guess_input_length
+            guess_key = guess_input.split()[0]
+            client_guess = guess_input.split()[1]
+            guess_input_length = len(guess_input.split())
+            if guess_key != "/guess" or guess_input_length != 2 or client_guess not in choices:
+                print("4002 Unrecognized message")
+        # Communicates with server
         guess_msg = f"{guess_input} {client_msg.split()[1]}"
         print(guess_msg)
         self.client_socket.send(guess_msg.encode())
         rcved_result_msg = self.client_socket.recv(1024).decode()
         print(rcved_result_msg)
 
+    def main(self):
+        # Client starts to connect to the server
+        try:
+            self.connect()
+            self.authenticate()
+            # After authentication success
+            while True:
+                try:
+                    # Get input from client
+                    client_msg = input("")
+                    # Extract the keyword (/list, /enter, /guess, ...) from the client input
+                    client_msg_key = client_msg.split()[0]
+                except Exception as error:
+                    print(error)
 
-    def run(self):
-        self.connect()
-        self.authenticate()
-
-        while True:
-            client_msg = input("")
-            client_msg_key = client_msg.split()[0]
-
-            if client_msg_key == "/exit":
-                self.client_socket.send(client_msg.encode())
-                rcved_msg = self.client_socket.recv(1024).decode()
-                print(rcved_msg)
-                break
-
-            elif client_msg_key == "/list":
-                self.client_socket.send(client_msg.encode())
-                rcved_msg = self.client_socket.recv(1024).decode()
-                print(rcved_msg)
-
-            elif client_msg_key == "/enter":
-                self.client_socket.send(client_msg.encode())
-                rcved_msg = self.client_socket.recv(1024).decode()
-                print(rcved_msg)
-
-                rcved_msg_key = rcved_msg.split()[0]
-                if rcved_msg_key == "3011":
+                # 2. In the Game Hall
+                # 1) list
+                if client_msg_key == "/list":
+                    self.client_socket.send(client_msg.encode())
                     rcved_msg = self.client_socket.recv(1024).decode()
                     print(rcved_msg)
-                    if rcved_msg.split()[0] == "3012":
+                # 2) enter
+                elif client_msg_key == "/enter":
+                    self.client_socket.send(client_msg.encode())
+                    rcved_msg = self.client_socket.recv(1024).decode()
+                    print(rcved_msg)
+
+                    rcved_msg_key = rcved_msg.split()[0]
+                    # First player to enter a room
+                    if rcved_msg_key == "3011":
+                        rcved_msg = self.client_socket.recv(1024).decode()
+                        print(rcved_msg)
+                        # When another player enters room, start a game
+                        if rcved_msg.split()[0] == "3012":
+                            self.guess(client_msg=client_msg)
+                    # Two players entered a room, start a game
+                    elif rcved_msg_key == "3012":
                         self.guess(client_msg=client_msg)
-                elif rcved_msg_key == "3012":
-                    self.guess(client_msg=client_msg)
+                    # Room is full
+                    else:
+                        continue
+                
+                # 4. Exit from the System
+                elif client_msg_key == "/exit":
+                    self.client_socket.send(client_msg.encode())
+                    rcved_msg = self.client_socket.recv(1024).decode()
+                    print(rcved_msg)
+                    self.client_socket.close()
+                    break
+
                 else:
-                    continue
+                    self.client_socket.send(client_msg.encode())
+                    rcved_msg = self.client_socket.recv(1024).decode()
+                    print(rcved_msg)
 
-            else:
-                self.client_socket.send(client_msg.encode())
-                rcved_msg = self.client_socket.recv(1024).decode()
-                print(rcved_msg)
-
-            # print(rcved_msg)
-            print("SENT:", client_msg)
-
-        self.client_socket.close()
+        except socket.error as emsg:
+            print("Socket error:", emsg)
+        finally:
+            self.client_socket.close()
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
@@ -95,4 +131,4 @@ if __name__ == '__main__':
     server_port = sys.argv[2]
 
     client = GameClient(server_addr, server_port)
-    client.run()
+    client.main()
--- Game.py
+++ Game.py
@@ -1,133 +1,197 @@
-import os
 import socket
 import sys
-import time
 import random
 from _thread import *
 import threading
 
+
 class GameServer:
+    # Initialize variables
     def __init__(self, num_of_rooms, file_path):
+        # Handles thread lock
         self.thrd_lock = threading.Lock()
-        self.connected_sockets = []
-        self.connected_users = []
-        self.num_of_rooms = num_of_rooms
+        # Path of a txt file containing username and password of clients
         self.file_path = file_path
+        # Stores client states, 0: in the game hall, 1: waiting in a room, 2: playing a game
+        self.connected_sockets = {}
+        # Number of rooms
+        self.num_of_rooms = num_of_rooms
+        # Stores which clients are waiting / playing in each game rooms
         self.room_list = [[] for _ in range(num_of_rooms)]
+        # Stores the guess made by clients in each game rooms
         self.guess_by_room = [{} for _ in range(num_of_rooms)]
+        # Stores username, password inputted by clients
         self.user_info = {}
 
+    # Reads username / password from UserInfo.txt and stores them into user_info dictionary
     def getUserInfo(self):
         with open(self.file_path) as txt:
             for line in txt:
                 username, pw = line.split(":")
                 self.user_info[username] = pw.strip()
 
-    def threaded(self, client_socket):
-        while True:
-            rcved_msg = client_socket.recv(1024).decode().split()
-            print("RCVED:", rcved_msg)
-            # authentication
-            if rcved_msg[0] == "/login":
-                username = rcved_msg[1]
-                password = rcved_msg[2]
-                if username not in self.user_info or username in self.connected_users or self.user_info[username] != password:
-                    msg = "1002 Authentication failed"
-                    client_socket.send(msg.encode())
-                else:
-                    msg = "1001 Authentication successful"
-                    client_socket.send(msg.encode())
-                    self.connected_sockets.append(client_socket)
-                    self.connected_users.append(username)
+    # Client thread
+    def client_thrd(self, client_socket):
+        try:
+            while True:
+                # Receive messages from client
+                rcved_msg = client_socket.recv(1024).decode().split()
+                if rcved_msg:
+                    # 1. User Authentication
+                    if rcved_msg[0] == "/login":
+                        username = rcved_msg[1]
+                        password = rcved_msg[2]
+                        # Authentication failed
+                        # Username does not exist or incorrect password
+                        if username not in self.user_info or self.user_info[username] != password:
+                            msg = "1002 Authentication failed"
+                            client_socket.send(msg.encode())
+                        # Authentication successful
+                        else:
+                            msg = "1001 Authentication successful"
+                            client_socket.send(msg.encode())
+                            # Client state: In the game hall
+                            self.connected_sockets[client_socket] = 0
 
-            # In the Game Hall
-            elif rcved_msg[0] == "/list":
-                self.thrd_lock.acquire()
-                str_list = [str(len(num)) for num in self.room_list]
-                msg = f"3001 {self.num_of_rooms} {' '.join(str_list)}"
-                client_socket.send(msg.encode())
-                self.thrd_lock.release()
+                    # 2. In the Game Hall
+                    # 1) list
+                    elif rcved_msg[0] == "/list":
+                        self.thrd_lock.acquire()
+                        # Convert a list to string format
+                        str_list = [str(len(num)) for num in self.room_list]
+                        # Message to be sent to client
+                        msg = f"3001 {self.num_of_rooms} {' '.join(str_list)}"
+                        client_socket.send(msg.encode())
+                        self.thrd_lock.release()
 
-            # enter
-            elif rcved_msg[0] == "/enter":
-                self.thrd_lock.acquire()
-                if len(self.room_list[int(rcved_msg[1]) - 1]) == 0:
-                    self.room_list[int(rcved_msg[1]) - 1].append(client_socket)
-                    msg = "3011 Wait"
-                    client_socket.send(msg.encode())
-                elif len(self.room_list[int(rcved_msg[1]) - 1]) == 1:
-                    self.room_list[int(rcved_msg[1]) - 1].append(client_socket)
-                    msg = "3012 Game started. Please guess true or false"
-                    for s in self.room_list[int(rcved_msg[1]) - 1]:
-                        s.send(msg.encode())
-                else:
-                    msg = "3013 The room is full"
-                    client_socket.send(msg.encode())
-                self.thrd_lock.release()
-
-            # guess
-            elif rcved_msg[0] == "/guess":
-                choices_list = ["true", "false"]
-                server_choice = random.choice(choices_list)
-
-                self.thrd_lock.acquire()
-                if len(self.guess_by_room[int(rcved_msg[2]) - 1]) < 2: 
-                    self.guess_by_room[int(rcved_msg[2])-1][client_socket] = rcved_msg[1]
-                self.thrd_lock.release()
-                self.thrd_lock.acquire()
-                if len(self.guess_by_room[int(rcved_msg[2]) - 1]) == 2:
-                    # Game Tied
-                    if len(set(self.guess_by_room[int(rcved_msg[2]) - 1].values())) == 1:
-                        msg = "3023 The result is tie"
-                        for s in self.guess_by_room[int(rcved_msg[2]) - 1].keys():
-                            s.send(msg.encode())
-                    # Game finished
-                    else:
-                        win_msg = "3021 You are the winner"
-                        loss_msg = "3022 You lost this game"
-                        for s, val in self.guess_by_room[int(rcved_msg[2])-1].items():
-                            if val == server_choice:
-                                s.send(win_msg.encode())
+                    # 2) enter
+                    elif rcved_msg[0] == "/enter":
+                        room_number = int(rcved_msg[1]) - 1
+                        # Room does not exist
+                        if room_number >= self.num_of_rooms:
+                            msg = "4002 Unrecognized message"
+                            client_socket.send(msg.encode())
+                        else:
+                            self.thrd_lock.acquire()
+                            # First player to enter a room
+                            if len(self.room_list[room_number]) == 0:
+                                self.room_list[room_number].append(client_socket)
+                                msg = "3011 Wait"
+                                client_socket.send(msg.encode())
+                                # Client state: Waiting in a room
+                                self.connected_sockets[client_socket] = 1
+                            # Two players entered a room
+                            elif len(self.room_list[room_number]) == 1:
+                                self.room_list[room_number].append(client_socket)
+                                msg = "3012 Game started. Please guess true or false"
+                                for client in self.room_list[room_number]:
+                                    client.send(msg.encode())
+                                    # Client state: Playing a game
+                                    self.connected_sockets[client] = 2
+                            # Room is full
                             else:
-                                s.send(loss_msg.encode())
-                    self.room_list[int(rcved_msg[2]) - 1] = []
-                    self.guess_by_room[int(rcved_msg[2]) - 1] = {}
-                self.thrd_lock.release()
+                                msg = "3013 The room is full"
+                                client_socket.send(msg.encode())
+                            self.thrd_lock.release()
 
-                print(self.guess_by_room)
+                    # 3. Playing a Game
+                    elif rcved_msg[0] == "/guess":
+                        # Room number client is playing
+                        room_number = int(rcved_msg[2]) - 1
+                        # Server generates random choice between true and false
+                        choices_list = ["true", "false"]
+                        server_choice = random.choice(choices_list)
+                        # One player sent its choice(guess), still waiting for another to send
+                        self.thrd_lock.acquire()
+                        if len(self.guess_by_room[room_number]) < 2: 
+                            self.guess_by_room[room_number][client_socket] = rcved_msg[1]
+                        self.thrd_lock.release()
 
-            # exit
-            elif rcved_msg[0] == "/exit":
-                msg = "4001 Bye bye"
-                client_socket.send(msg.encode())
-                break
+                        self.thrd_lock.acquire()
+                        # Both players sent their choice(guess)
+                        if len(self.guess_by_room[room_number]) == 2:
+                            # Game Tied
+                            if len(set(self.guess_by_room[room_number].values())) == 1:
+                                msg = "3023 The result is tie"
+                                for client in self.guess_by_room[room_number].keys():
+                                    client.send(msg.encode())
+                            # Game Not Tied
+                            else:
+                                win_msg = "3021 You are the winner"
+                                loss_msg = "3022 You lost this game"
+                                for client, client_guess in self.guess_by_room[room_number].items():
+                                    # Winner
+                                    if client_guess == server_choice:
+                                        client.send(win_msg.encode())
+                                    # Loser
+                                    else:
+                                        client.send(loss_msg.encode())
+                                    # Client state: In the game hall
+                                    self.connected_sockets[client] = 0
+                            # Reset room_list, guess_by_room
+                            self.room_list[room_number] = []
+                            self.guess_by_room[room_number] = {}
+                        self.thrd_lock.release()
 
-            else:
-                msg = "4002 Unrecognized message"
-                client_socket.send(msg.encode())
+                    # 4. Exit from the System
+                    elif rcved_msg[0] == "/exit":
+                        msg = "4001 Bye bye"
+                        client_socket.send(msg.encode())
+                        self.connected_sockets.pop(client_socket, None)
+                        break
 
-            print("Connected Sockets #:", len(self.connected_sockets))
+                    # 5. Dealing with incorrect message format
+                    else:
+                        msg = "4002 Unrecognized message"
+                        client_socket.send(msg.encode())
 
+                else:
+                    client_socket.close()
+        except ConnectionResetError:
+            print("Client connection was reset.")
+        except ConnectionAbortedError:
+            print("Client connection was aborted.")
+        except socket.error as emsg:
+            print("Socket error:", emsg)
+        finally:
+            # Handle exception when one player is disconnected while two players were playing
+            if self.connected_sockets[client_socket] == 2:
+                for index, room in enumerate(self.room_list):
+                    if client_socket in room:
+                        room_num = index
+                        break
+                self.room_list[room_num].remove(client_socket)
+                msg = "3021 You are the winner"
+                self.room_list[room_num][0].send(msg.encode())
+                self.connected_sockets[self.room_list[room_num][0]] = 0
+                self.room_list[room_num] = []
+                self.guess_by_room[room_num] = {}
+            
+            # Reset the connected_sockets
+            self.connected_sockets.pop(client_socket, None)
 
     def main(self, server_port):
+        # Start the server
         try:
             server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             server_socket.bind(("", server_port))
+            server_socket.listen(5)
+
+            print("Hello world")
+            # Creates a new socket and a new thread
+            while True:
+                try:
+                    client_socket, addr = server_socket.accept()
+                except socket.error as emsg:
+                    print("socket error:", emsg)
+                # Starts a new thread that handles client socket
+                else:
+                    start_new_thread(self.client_thrd, (client_socket,))
+
         except socket.error as emsg:
             print("Socket error:", emsg)
-
-        server_socket.listen(5)
-
-        print("Hello world")
-
-        while True:
-            try:
-                client_socket, addr = server_socket.accept()
-            except socket.error as emsg:
-                print("socket error:", emsg)
-            start_new_thread(self.threaded, (client_socket,))
-            print("addr:", addr)
-
+        finally:
+            server_socket.close()
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
